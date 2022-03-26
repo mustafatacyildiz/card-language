ed61509b141e0adff4ebb510b634669a5b066e30
Mustafa TAÇYILDIZ <93773148+D1422-Mustafa@users.noreply.github.com>
2022-03-26 18:28:22 +0300

    fifth commit

--- src/component/item/Item.js
+++ src/component/item/Item.js
@@ -1,13 +1,25 @@
-import "./Item.css"
+import { useState } from "react";
+import "./Item.css";
 
 const Item = ({ card }) => {
-    return (
-      <div className="card">
-        <img className="card-logo" src={card.img} alt="" />
-        <h3 className="card-title">{card.name}</h3>
-        {/* {console.log(card)} */}
-      </div>
-    );
-  };
-  
-  export default Item;
+  const [showLogo, setShowLogo] = useState(true);
+
+  return (
+    <div className="card" onClick={() => setShowLogo(!showLogo)}>
+      {showLogo ? (
+        <div>
+          <img className="card-logo" src={card.img} alt="logo" />
+          <h3 className="card-title">{card.name}</h3>
+        </div>
+      ) : (
+        <ul className="list">
+          {card.options.map((element, index) => {
+            return <li key={index}>{element}</li>;
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Item;
